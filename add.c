61ff13fcd3b02487194745fc42aabe2a7b280dea
eddy <106661971+eddiemws@users.noreply.github.com>
2024-05-28 21:46:14 +0300

    Update add.c

--- add.c
+++ add.c
@@ -8,4 +8,27 @@
  * Return: pointer to the newly added node
  */
 stack_t *add_node(stack_t **stack, const int value)
+{
+	stack_t *newNode;
+
+	newNode = malloc(sizeof(stack_t));
+	if (newNode == NULL)
+	{
+		print_error_and_exit("malloc failed", stack);
+	}
+
+	newNode->n = value;
+	newNode->prev = NULL;
+
+	if (*stack == NULL)
+		newNode->next = NULL;
+	else
+	{
+		newNode->next = *stack;
+		(*stack)->prev = newNode;
+	}
+	*stack = newNode;
+
+	return (newNode);
+}
 
